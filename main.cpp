fce8cbae08c7b32c7e42f102f35f059f9a556c83
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2020-12-30 12:15:36 +0100

    Enhance the output

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@
 int main()
 {
     auto window = sf::RenderWindow{ {1024, 720}, "SFML Input Test" };
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(15);
+
+    for (auto i = 0; i < sf::Keyboard::ScanCodeCount; ++i)
+    {
+        std::cout << i << ":\t" << sf::Keyboard::getDescription(static_cast<sf::Keyboard::Scancode>(i)).toAnsiString() << '\n';
+    }
+
+    std::cout << '\n';
 
     while (window.isOpen())
     {
@@ -16,14 +23,30 @@ int main()
             }
             else if(event.type == sf::Event::KeyPressed)
             {
-                std::cout << "Code: " << std::to_string(event.key.code) << '\n';
-                std::cout << "ScanCode: " << std::to_string(event.key.scancode) << '\n';
+                std::cout << "Key Pressed\n";
+                std::cout << "Code:\t\t" << std::to_string(event.key.code) << '\n';
+                std::cout << "ScanCode:\t" << std::to_string(event.key.scancode) << '\n';
+                std::cout << "Description:\t" << sf::Keyboard::getDescription(event.key.scancode).toAnsiString() << '\n';
                 std::cout << '\n';
             }
             else if (event.type == sf::Event::KeyReleased)
             {
-                std::cout << "Code: " << std::to_string(event.key.code) << '\n';
-                std::cout << "ScanCode: " << std::to_string(event.key.scancode) << '\n';
+                std::cout << "Key Released\n";
+                std::cout << "Code:\t\t" << std::to_string(event.key.code) << '\n';
+                std::cout << "ScanCode:\t" << std::to_string(event.key.scancode) << '\n';
+                std::cout << "Description:\t" << sf::Keyboard::getDescription(event.key.scancode).toAnsiString() << '\n';
+                std::cout << '\n';
+            }
+            else if (event.type == sf::Event::MouseButtonPressed)
+            {
+                std::cout << "Mouse Button Pressed\n";
+                std::cout << "Button:\t" << std::to_string(event.mouseButton.button) << '\n';
+                std::cout << '\n';
+            }
+            else if (event.type == sf::Event::MouseButtonReleased)
+            {
+                std::cout << "Mouse Button Released\n";
+                std::cout << "Button:\t" << std::to_string(event.mouseButton.button) << '\n';
                 std::cout << '\n';
             }
         }
